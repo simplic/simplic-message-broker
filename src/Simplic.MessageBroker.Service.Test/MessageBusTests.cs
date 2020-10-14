6ae1ae7ebb41c89b1977f1943a443b2d081cd36c
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2020-10-14 11:42:08 +0200

    Fix test

--- src/Simplic.MessageBroker.Service.Test/MessageBusTests.cs
+++ src/Simplic.MessageBroker.Service.Test/MessageBusTests.cs
@@ -2,7 +2,6 @@
 using Moq;
 using Simplic.MessageChannel;
 using Simplic.Session;
-using System;
 using Unity;
 using Xunit;
 
@@ -11,20 +10,26 @@ namespace Simplic.MessageBroker.Service.Test
     public class MessageBusTests
     {
         [Fact]
-        public void MessageBusSend_UsingGenericAndAnonymus_SendsMessageIdAndUser()
+        public void MessageBusSend_UsingGenericAndAnonymus_IncrementsUserAndClobalTaskQueue()
         {
             var container = new UnityContainer();
 
             container.RegisterType<IMessageBus, MessageBus>();
 
-            var channelPublisher = new Mock<IChannelPublisher>();
-            var publishCalled = 0;
-            channelPublisher.Setup(x => x.PublishAsync(It.IsAny<string>(), It.Is<string>(y => y.Contains("MessageId") && y.Contains("UserId"))))
+            #region Mock IMessageChannel
+
+            var messageChannel = new Mock<IMessageChannel>();
+            var incrementCalled = 0;
+            messageChannel.Setup(x => x.StringIncrement(It.IsAny<string>(), It.IsAny<double>()))
                 .Callback(() =>
                 {
-                    publishCalled += 1;
+                    incrementCalled += 1;
                 });
-            container.RegisterInstance<IChannelPublisher>(channelPublisher.Object);
+            container.RegisterInstance<IMessageChannel>(messageChannel.Object);
+
+            #endregion Mock IMessageChannel
+
+            #region Mock ISessionService
 
             var sessionService = new Mock<ISessionService>();
             sessionService.Setup(x => x.CurrentSession).Returns(() =>
@@ -36,16 +41,20 @@ namespace Simplic.MessageBroker.Service.Test
             });
             container.RegisterInstance<ISessionService>(sessionService.Object);
 
+            #endregion Mock ISessionService
+
             var busControl = new Mock<IBusControl>();
             container.RegisterInstance<IBusControl>(busControl.Object);
 
+            // Send a message via messagebus
             var messageBus = container.Resolve<IMessageBus>();
-            messageBus.Send<TestInterface>(new { Name = "TestName", Value = "TestValue", MessageId = Guid.NewGuid() });
+            messageBus.Send<TestInterface>(new { Name = "TestName", Value = "TestValue" });
 
-            Assert.Equal(1, publishCalled);
+            //Since the increment is called once for the user and once for the global queue it needs to be 2
+            Assert.Equal(2, incrementCalled);
         }
 
-        public interface TestInterface : ICommandBase
+        public interface TestInterface
         {
             string Name { get; set; }
             string Value { get; set; }
